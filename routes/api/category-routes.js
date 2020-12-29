c22728b3c6860bfbb6c7e30cbaa65d19a24b4667
Adam Barron <adambarron@me.com>
2020-12-28 23:18:43 -0800

    cleaned up a few errors with quotation marks, added both find routes in cateogry-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,50 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id', 
+      'category_name'
+    ],
+    include: [
+      {
+        model: Products,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Products,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
 });
 
 router.post('/', (req, res) => {
